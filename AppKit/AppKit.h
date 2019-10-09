7a0cc32a621aa38a3ece39daf16768b9c99b0dd2
Tran Khiem <tuoitrevohoc@me.com>
2019-10-08 20:01:10 -0700

    Initial Commit

---
+++ AppKit/AppKit.h
@@ -0,0 +1,19 @@
+//
+//  AppKit.h
+//  AppKit
+//
+//  Created by Tran Thien Khiem on 2019-10-08.
+//  Copyright © 2019 Tran Thien Khiem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppKit.
+FOUNDATION_EXPORT double AppKitVersionNumber;
+
+//! Project version string for AppKit.
+FOUNDATION_EXPORT const unsigned char AppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppKit/PublicHeader.h>
+
+
